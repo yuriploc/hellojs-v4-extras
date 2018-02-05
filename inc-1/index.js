62375aea2b823a316c9c89c2f8cd797aef0a1de0
Yuri Oliveira <soaresyuri@gmail.com>
2018-02-05 10:27:12 -0300

    WIP on inc-1

--- inc-1/index.js
+++ inc-1/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const axios = require('axios')
+const knex = require('./config').knex
 
 const app = express()
 
 const catApi = axios.create({
-  baseURL: 'http://thecatapi.com'
+  baseURL: 'http://thecatapi.com/api'
 })
 
 const onCatch = res => err => {
@@ -12,7 +13,22 @@ const onCatch = res => err => {
   res.status(500).send(err)
 }
 
-// TODO: get the kitten img and insert it
-// app.get()
+const params = {
+  format: 'html',
+  api_key: process.env.CAT_API_KEY
+}
 
-app.listen(3000, _ => console.log(`UP!`))
+app.get('/my-kittens', (req, res) =>
+  catApi
+    .get('/images/get', { params })
+    .then(ret => {
+      const regexp = /\?id\=\w+/g
+      const omg = ret.data.match(regexp)
+      const imgId = omg[0].split('=')[1]
+      // TODO: insert/update
+      res.status(200).send(ret.data)
+    })
+    .catch(onCatch(res))
+)
+
+knex.migrate.latest().then(_ => app.listen(3000, _ => console.log(`UP!`)))
